064e73d5c3b7c676161588a2c55b79561fe8fbcc
Evgeny <ahotickkk@mail.ru>
2020-09-01 14:46:30 +0300

    Edit section-specefication

--- #src/js/custom.js
+++ #src/js/custom.js
@@ -69,3 +69,42 @@ let tab = function () {
 tab();
 
 // Tabs end
+
+// Dots dynamics start
+
+(function () {
+    var self = $('.c-section-build');
+    var dots = self.find('.c-section-build__scheme__dots');
+    var nav = self.find('.c-section-build__scheme__nav');
+
+    function setActive(index) {
+        dots.find('li').eq(index).addClass('is-active').siblings().removeClass('is-active');
+        nav.find('li').eq(index).addClass('is-active').siblings().removeClass('is-active');
+        $("#с-build__active-text").find('p').eq(index).addClass('is-active').siblings().removeClass('is-active');
+    }
+
+    dots.find('li').on('click', function () {
+        setActive($(this).index());
+    });
+    nav.find('li').on('click', function () {
+        setActive($(this).index());
+    });
+
+})();
+
+// Dots dynamics end
+
+// Spoiler start
+
+
+$(document).ready(function() {
+	$('.block__title').click(function(event) {
+		if($('.block').hasClass('one')){
+			$('.block__title').not($(this)).removeClass('active');
+			$('.block__text').not($(this).next()).slideUp(300);
+		}
+		$(this).toggleClass('active').next().slideToggle(300);
+	});
+});
+
+// Spoiler end
