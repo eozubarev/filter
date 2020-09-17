25639e04a0b5c53311a99ec41bc588dd9e64ed65
“Evgeny <ahotickkk@mail.ru>
2020-09-17 15:04:33 +0300

    edit size 'deep-css-element'

---
+++ #src/js/scheme.js
@@ -0,0 +1,43 @@
+
+// Dots dynamics start
+
+(function () {
+    var self = $('.section-build');
+    var dots = self.find('.c-section-build__scheme__dots');
+	var nav = self.find('.block__item');
+	
+    function setActive(index) {
+		dots.find('li').eq(index).addClass('is-active').siblings().removeClass('is-active');
+		nav.find('li').eq(index).addClass('is-active').siblings().removeClass('is-active');
+		
+		// console.log(nav.children(".block__title").eq(index).addClass('is-active'));
+
+		// console.log( nav.find('.block__title').eq(index).addClass('is-active') );
+
+	}
+
+    dots.find('li').on('click', function () {
+        setActive($(this).index());
+    });
+    nav.on('click', function () {
+        setActive($(this).index());
+	});
+
+})();
+
+
+// Dots dynamics end
+
+// Spoiler start
+
+$(document).ready(function() {
+	$('.block__title').click(function(event) {
+		if($('.block').hasClass('one')){
+			$('.block__title').not($(this)).removeClass('active');
+			$('.block__text').not($(this).next()).slideUp(300);
+		}
+		$(this).toggleClass('active').next().slideToggle(300);
+	});
+});
+
+// Spoiler end
--- #src/js/script.js
+++ #src/js/script.js
@@ -1,4 +1,9 @@
-@@include('custom.js');
-@@include('dynamic.js');
+@@include('common.js');
 @@include('headerScroll.js');
 @@include('buttonUp.js');
+@@include('scheme.js');
+@@include('tabs.js');
+@@include('readMoreBtn.js');
+@@include('popup.js');
+@@include('myLib.js');
+@@include('scrollTo.js');
