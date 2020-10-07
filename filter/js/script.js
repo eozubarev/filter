75a9bf970d417043d1c4afa1722b54991a4319ea
“Evgeny <ahotickkk@mail.ru>
2020-10-07 12:01:09 +0300

    edit certificates block

--- filter/js/script.js
+++ filter/js/script.js
@@ -1,18 +1,4 @@
 //Прелоадер для сайта
-function loadData() {
-    return new Promise((resolve, reject) => {
-      // setTimeout не является частью решения
-      // Код ниже должен быть заменен на логику подходящую для решения вашей задачи
-      setTimeout(resolve, 700);
-    })
-  }
-  
-  loadData()
-    .then(() => {
-      let preloaderEl = document.getElementById('cube-loader');
-      preloaderEl.classList.add('hidden');
-      preloaderEl.classList.remove('visible');
-    });;
 
 // в myLib лежат кастомные скрипты
   /* myLib */
